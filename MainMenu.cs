bf58b3e047d15b612bee21a5a4e32351e8fac087
sharow11 <sharow1234@gmail.com>
2023-09-17 22:04:18 +0200

    Działa wychodzenie z gry

--- MainMenu.cs
+++ MainMenu.cs
@@ -3,14 +3,26 @@ using System;
 
 public partial class MainMenu : Node2D
 {
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+	}
+	// Called when the node enters the scene tree for the first time.
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var button = new Button();
-		button.Text = "Click me";
+		button.Text = "Nowa Gra";
 		button.Pressed += ButtonPressed;
-		button.Position = Position with { X = 100.0f,  Y = 100.0f  };
+		button.Position = Position with { X = 200.0f,  Y = 200.0f  };
 		AddChild(button);
+
+		var button2 = new Button();
+		button2.Text = "Wyjście";
+		button2.Pressed += ButtonExit;
+		button2.Position = Position with { X = 200.0f,  Y = 300.0f  };
+		AddChild(button2);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,5 +33,10 @@ public partial class MainMenu : Node2D
 	private void ButtonPressed()
 	{
 		GD.Print("Hello world!");
+	}
+
+	private void ButtonExit()
+	{
+		GetTree().Quit();
 	}
 }
